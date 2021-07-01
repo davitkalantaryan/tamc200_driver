15598219311da32c7fdbe40f2bc520cd73be9580
Davit Kalantaryan <davit.kalantaryan@desy.de>
2021-07-01 15:40:19 +0200

    Feature/4 test mmap (#5)
    * added user space app test for testing mmap functionality
    * fixed mmap test and tested it

---
+++ tests/main_mmap_test.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pciedev_io.h>
+
+#define mmap_len_bar    4096
+#define def_nod_name    "/dev/tamc200s5"
+
+
+int main(int a_argc, char* a_argv[])
+{
+        const char* nod_name = def_nod_name;
+        u_int    tmp_barx;
+        uint16_t regVal;
+        int fd = -1;
+        void* mmap_address=MAP_FAILED;
+        unsigned long mmap_offset;
+
+        printf("mmap test. verson 6. MMAP_BAR_SHIFT=%d\n",(int)MMAP_BAR_SHIFT);
+
+        if(a_argc<2){
+                fprintf(stderr,"provide bar number to map\n");
+                return 1;
+        }
+        tmp_barx = (u_int)atoi(a_argv[1]);
+	mmap_offset = (unsigned long)(tmp_barx<< MMAP_BAR_SHIFT);
+
+        if(a_argc>2){
+                nod_name = a_argv[2];
+        }
+        printf("node_name=%s, bar=%d\n",nod_name,(int)tmp_barx);
+
+        fd = open (nod_name, O_RDWR);
+        printf("fd=%d\n",fd);
+        if(fd<0){
+                fprintf(stderr,"Unable to open device %s \n",nod_name);
+                return 1;
+        }
+
+        mmap_address = mmap(0, mmap_len_bar, PROT_READ | PROT_WRITE, MAP_SHARED, fd, mmap_offset);
+	//mmap_address = mmap(0, mmap_len_bar, PROT_READ | PROT_WRITE, MAP_SHARED, fd, tmp_barx?4096<<(tmp_barx-1):0);
+	//mmap_address = mmap(0, mmap_len_bar, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 4096*tmp_barx);
+        printf("mapped address: %p\n",mmap_address);
+
+        if(mmap_address==MAP_FAILED){
+                close(fd);
+                fprintf(stderr,"Unable to map device " "%s" " bar %d\n",nod_name,tmp_barx);
+                return 1;
+        }
+
+        regVal = ((uint16_t*)mmap_address)[0];
+        printf("regVal=%d\n",(int)regVal);
+        ((uint16_t*)mmap_address)[0] = regVal;
+        munmap(mmap_address, mmap_len_bar);
+        close(fd);
+
+        return 0;
+}
+
